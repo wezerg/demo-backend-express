86936f0875e31912415ed8e9a3c48c6360ea1d20
wezerg <vivian.grimaux@hotmail.fr>
2022-05-23 16:41:05 +0200

    Modification du code pour inclure MongoDB Remplacement de l'ancienne bdd

--- app.js
+++ app.js
@@ -5,11 +5,33 @@ const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-// Bdd
+// Fake Bdd
 const DbSet = require('./db.js');
 const Taches = new DbSet('Taches');
 const Users = new DbSet('Users');
+// Mongo Bdd
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/eval')
+    .then(() => console.log('Connected to mango'))
+    .catch((err) => console.log("Not connected to database", err));
 
+// Schéma et classes Mongoose
+const userSchema = new mongoose.Schema({
+    id: Number,
+    username: String,
+    email: String,
+    password: String
+});
+const User = mongoose.model('User', userSchema);
+const taskSchema = new mongoose.Schema({
+    id: Number,
+    description: String,
+    faite: Boolean,
+    creePar: Number
+});
+const Task = mongoose.model('Task', taskSchema);
+
+// Check variables d'environnements
 if (!process.env.SECRET_KEY) {
     console.error("Error : Variable d'environnement 'SECRET_KEY' inexistante -- Exit Program");
     process.exit(1);
@@ -33,21 +55,25 @@ function authenticationToken(req, res, next){
 }
 
 // Routes Taches
-app.get('/task', (req, res) => {
-    const taches = Taches.getAll();
+app.get('/task', async (req, res) => {
+    const taches = await Task.find();
+    //const taches = Taches.getAll();
     res.status(200).send(taches);
 });
-app.get('/task/:id', (req, res) => {
-    const tache = Taches.get(parseInt(req.params.id));
+app.get('/task/:id', async (req, res) => {
+    const tache = await Task.findOne({id: parseInt(req.params.id)});
+    //const tache = Taches.get(parseInt(req.params.id));
     if (!tache) {
         throw new Error('Aucune tache trouvée');
     }
     res.status(200).send(tache);
 });
 
-app.post('/task', [authenticationToken], (req, res) => {
+app.post('/task', [authenticationToken], async (req, res) => {
+    //return await user.save();
     const payload = req.body;
     const scheme = Joi.object({
+        id: Joi.allow(),
         description: Joi.string().max(255).required(),
         faite: Joi.boolean().required()
     });
@@ -56,10 +82,12 @@ app.post('/task', [authenticationToken], (req, res) => {
         throw new Error(error.details[0].message);
     }
     value.creePar = req.user.id;
-    Taches.insert(value);
+    const task = new Task(value);
+    //Taches.insert(value);
+    await task.save();
     res.status(201).send(value);
 });
-app.put('/task/:id', [authenticationToken], (req, res) => {
+app.put('/task/:id', [authenticationToken], async (req, res) => {
     if (parseInt(req.params.id) === NaN) {
         throw new Error("Ceci n'est pas une clé valable");
     }
@@ -72,19 +100,25 @@ app.put('/task/:id', [authenticationToken], (req, res) => {
     if (error) {
         throw new Error(error.details[0].message);
     }
-    Taches.update(parseInt(req.params.id), value);
-    res.status(200).send(value);
+    const tache = await Task.findOne({id: parseInt(req.params.id)});
+    tache.description = payload.description;
+    tache.faite = payload.faite;
+    const modified = await tache.save();
+    //Taches.update(parseInt(req.params.id), value);
+    res.status(200).send(modified);
 });
 
-app.delete('/task/:id', [authenticationToken], (req, res) => {
+app.delete('/task/:id', [authenticationToken], async (req, res) => {
     if (parseInt(req.params.id) === NaN) {
         throw new Error("Ceci n'est pas une clé valable");
     }
-    const tache = Taches.get(parseInt(req.params.id));
+    //const tache = Taches.get(parseInt(req.params.id));
+    const tache = await Task.findOne({id: parseInt(req.params.id)});
     if (parseInt(req.user.id) !== parseInt(tache.creePar)) {
         throw new Error("Vous ne pouvez pas supprimer la tâche d'un autre utilisateur");
     }
-    Taches.delete(parseInt(req.params.id));
+    await Task.deleteOne({id: parseInt(req.params.id)})
+    //Taches.delete(parseInt(req.params.id));
     res.status(200).send("Ressources supprimée");
 });
 
@@ -93,6 +127,7 @@ app.post('/register', async (req, res) => {
     const payload = req.body;
     // Joi
     const scheme = Joi.object({
+        id: Joi.allow(),
         email: Joi.string().max(255).email().required(),
         username: Joi.string().max(255).required(),
         password: Joi.string().max(255).required()
@@ -102,17 +137,17 @@ app.post('/register', async (req, res) => {
         throw new Error(error.details[0].message);
     }
     // Vérification d'un compte existant en base de données
-    const {entity} = Users.findBy('email', value.email);
-    if (entity) {
+    //const {entity} = Users.findBy('email', value.email);
+    const user = await User.findOne({email: value.email});
+    if (user) {
         throw new Error("Un compte existe déja");
     }
-    // Hash du mot de passe
     const salt = await bcrypt.genSalt(10);
     const passwordHash = await bcrypt.hash(value.password, salt);
     value.password = passwordHash;
-    // Enregistrement en bdd
-    Users.insert(value);
-    // Renvoit
+    const userCreated = new User(value);
+    await userCreated.save();
+    //Users.insert(value);
     res.status(201).send({email: value.email, username: value.username});
 });
 
@@ -126,15 +161,16 @@ app.post('/login', async (req, res) => {
     if (error) {
         throw new Error(error.details[0].message);
     }
-    const {id, entity} = Users.findBy('email', value.email);
-    if (!entity) {
+    const user = await User.findOne({email: value.email});
+    //const {id, entity} = Users.findBy('email', value.email);
+    if (!user) {
         throw new Error("Ce compte n'existe pas");
     }
-    const goodHash = await bcrypt.compare(payload.password, entity.password);
+    const goodHash = await bcrypt.compare(payload.password, user.password);
     if (!goodHash) {
         throw new Error('Mot de passe invalide');
     }
-    const token = jwt.sign({id}, process.env.SECRET_KEY);
+    const token = jwt.sign({id: user.id}, process.env.SECRET_KEY);
     res.header('x-auth-token', token).status(200).send("Connexion réussie");
 });
 
