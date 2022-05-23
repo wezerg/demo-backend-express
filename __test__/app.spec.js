5935e4fd579bb064685cc65c4010d5d0072319ea
wezerg <vivian.grimaux@hotmail.fr>
2022-05-23 15:39:41 +0200

    Ajout du middleware d'authentification token Rectification des test pour prendre en compte les tokens utilisateurs sur les routes Create / Edit & Delete

--- __test__/app.spec.js
+++ __test__/app.spec.js
@@ -1,41 +1,46 @@
-const {app, Taches} = require('../app');
+const {app, Taches, Users} = require('../app');
 const request = require('supertest');
 
 afterEach(() => {
     Taches.memoryDb = new Map();
     Taches.id = 0;
+    Users.memoryDb = new Map();
+    Users.id = 0;
 });
 
 describe('Task', () => {
-    test.each([
-        {description: "Description 1", faite: true},
-        {description: "Description 2", faite: false},
-        {description: "Description 3", faite: true}
-    ])('Should pass confirmation data', async (objectTest) => {
-        const result = await request(app).post('/task').send(objectTest).expect(201);
+    test('Should pass confirmation data', async () => {
+        const register = await request(app).post('/register').send({email: "email@email.com", username: "vivian", password: "1234"}).expect(201);
+        const login = await request(app).post('/login').send({email: "email@email.com", password: "1234"}).expect(200);
+        const result = await request(app).post('/task').set('x-auth-token', login.headers['x-auth-token']).send({description: "Description 1", faite: true}).expect(201);
     });
     test('Should not find object', async () => {
         const result = await request(app).get('/task/136').send().expect(400);
     });
     test('Should find object', async () => {
-        const sendData = await request(app).post('/task').send({description: "Hello World", faite: false}).expect(201);
-        console.log(Taches.getAll());
+        const register = await request(app).post('/register').send({email: "email@email.com", username: "vivian", password: "1234"}).expect(201);
+        const login = await request(app).post('/login').send({email: "email@email.com", password: "1234"}).expect(200);
+        const sendData = await request(app).post('/task').set('x-auth-token', login.headers['x-auth-token']).send({description: "Hello World", faite: false}).expect(201); // Ici
         const result = await request(app).get('/task/0').send().expect(200);
     });
 });
 
 describe('Task Update', () => {
     test('Should update one entry', async () => {
-        const result = await request(app).post('/task').send({description: "Desciption 1", faite: false}).expect(201);
-        const modif = await request(app).put('/task/0').send({description: "Description1", faite: true}).expect(200);
+        const register = await request(app).post('/register').send({email: "email@email.com", username: "vivian", password: "1234"}).expect(201);
+        const login = await request(app).post('/login').send({email: "email@email.com", password: "1234"}).expect(200);
+        const result = await request(app).post('/task').set('x-auth-token', login.headers['x-auth-token']).send({description: "Desciption 1", faite: false}).expect(201);
+        const modif = await request(app).put('/task/0').set('x-auth-token', login.headers['x-auth-token']).send({description: "Description1", faite: true}).expect(200);
         expect(modif.body.faite).toBe(true);
     });
 });
 
 describe('Task Delete', () => {
     test('Should delete one entry', async () => {
-        const result = await request(app).post('/task').send({description: "Desciption 1", faite: false}).expect(201);
-        const modif = await request(app).delete('/task/0').send({description: "Description1", faite: true}).expect(200);
+        const register = await request(app).post('/register').send({email: "email@email.com", username: "vivian", password: "1234"}).expect(201);
+        const login = await request(app).post('/login').send({email: "email@email.com", password: "1234"}).expect(200);
+        const result = await request(app).post('/task').set('x-auth-token', login.headers['x-auth-token']).send({description: "Desciption 1", faite: false}).expect(201);
+        const modif = await request(app).delete('/task/0').set('x-auth-token', login.headers['x-auth-token']).send({description: "Description1", faite: true}).expect(200);
         expect(Taches.memoryDb.entries.length).toBe(0);
     });
 });
--- app.js
+++ app.js
@@ -12,8 +12,20 @@ const Users = new DbSet('Users');
 
 // Middleware
 app.use(express.json());
-
-
+// - Authentification
+function authenticationToken(req, res, next){
+    const token = req.header('x-auth-token');
+    if (!token) {
+        throw new Error('Vous devez vous connecté pour accéder à cette ressource');
+    }
+    try {
+        const userInToken = jwt.verify(token, process.env.SECRET_KEY);
+        req.user = userInToken;
+        next();
+    } catch (error) {
+        throw new Error('Le token que vous avez fourni est invalide');
+    }
+}
 
 // Routes Taches
 app.get('/task', (req, res) => {
@@ -28,7 +40,7 @@ app.get('/task/:id', (req, res) => {
     res.status(200).send(tache);
 });
 
-app.post('/task', (req, res) => {
+app.post('/task', [authenticationToken], (req, res) => {
     const payload = req.body;
     const scheme = Joi.object({
         description: Joi.string().max(255).required(),
@@ -41,7 +53,7 @@ app.post('/task', (req, res) => {
     Taches.insert(value);
     res.status(201).send(value);
 });
-app.put('/task/:id', (req, res) => {
+app.put('/task/:id', [authenticationToken], (req, res) => {
     if (parseInt(req.params.id) === NaN) {
         throw new Error("Ceci n'est pas une clé valable");
     }
@@ -58,7 +70,7 @@ app.put('/task/:id', (req, res) => {
     res.status(200).send(value);
 });
 
-app.delete('/task/:id', (req, res) => {
+app.delete('/task/:id', [authenticationToken], (req, res) => {
     if (parseInt(req.params.id) === NaN) {
         throw new Error("Ceci n'est pas une clé valable");
     }
@@ -125,4 +137,4 @@ app.use((err, req, res, next) => {
 if (process.env.NODE_ENV !== "test") {
     app.listen(3000);
 }
-module.exports = { app, Taches };
+module.exports = { app, Taches, Users };
